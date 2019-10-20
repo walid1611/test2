49492e4662864f4ddbad8e80ebd7beb9f00b6d56
walid <walidsaidi.ws@gmail.com>
2019-10-20 14:50:01 +0200

    ajouter des test

--- src/main/java/TEST2.java
+++ src/main/java/TEST2.java
@@ -3,6 +3,7 @@ public class TEST2 {
     public static void main(String[] args) {
         System.out.println("ok");
         System.out.println("ok2");
+        System.out.println("ok2");
 
     }
 }
